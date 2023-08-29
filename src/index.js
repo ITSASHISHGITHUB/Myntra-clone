1cf3b875b7845aa7582e6387335b9f9a08020ba9
Ashish Yadav <ay622012002@gmail.com>
2023-08-29 17:04:20 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import {Provider} from 'react-redux'
+import ToasterProvider from './Provider/ToastProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <ToasterProvider/>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/pages/ListPage.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../components/Layout/Layout";
+import Navbar from "../components/Navbar/Navbar";
+import ProductList from "../components/ProductList";
+import ComboBox from "../components/ComboBox";
+import "./ListPage.css";
+import Modal from "../components/Modal";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import {useDispatch} from 'react-redux'
+import { addProducts } from "../store/reducers/productsReducer";
+
+import {data} from "../data"
+
+const options = [
+  { value: "lth", label: "Low to High" },
+  { value: "htl", label: "High to Low" },
+];
+
+const ListPage = () => {
+  const products = useSelector(state=>state.products);
+  const dispatch=useDispatch()
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+
+          dispatch(addProducts(data)) 
+          setFilteredProducts(data)
+  }, []);
+  // console.log(products)
+  const handleSearchChange = (e) => {
+    const newSearchText = e.target.value;
+    setSearchText(newSearchText);
+
+    if (newSearchText === "") {
+      setFilteredProducts(products); // Set the original full list of products
+    } else {
+      const newProducts = products.filter((item) =>
+        item.title.includes(newSearchText)
+      );
+      setFilteredProducts(newProducts);
+    }
+  };
+  console.log(products)
+
+  return (
+    <div className="listpage-container">
+      <Navbar
+        setShowModal={setShowModal}
+        searchText={searchText}
+        handleSearchChange={handleSearchChange}
+      />
+<div className="basic-start">
+  Home / Clothing / <strong>Shirts For Men & Women</strong>
+</div>
+      {showModal && <Modal />}
+      <div className="basic-start"><strong>Shirts For Men & Women 
+        </strong>- 98665 items</div>
+      
+      <div className="basic-start1">
+        FILTERS
+        <ul className="list-details">
+          <li>
+            <a href="">Country of Origin</a>
+            </li>
+          <li>
+            <a href="">Clear all</a>
+          </li>
+          
+          <li>
+            <a href="">Add-Ons</a>
+          </li>
+          <li>
+            <a href="">Bundles</a>
+          </li>
+          <li>
+            <a href="">Collar</a>
+          </li>
+          <li>
+            <a href="">Fabric</a>
+          </li>
+          <li>
+            <a href="">Fit</a>
+          </li>
+          <div className="Combo-box">
+            <ComboBox options={options} setFilteredProducts={setFilteredProducts} products={products}/>
+          </div>
+
+        </ul>
+      </div>
+      <div className="App"></div>
+      <hr/>
+      <Layout setFilteredProducts={setFilteredProducts} products={products}>
+      {showModal && <Modal/>}
+        <h1>Shirt Collection</h1>
+        <ProductList products={filteredProducts} />
+      </Layout>
+    </div>
+  );
+};
+
+export default ListPage;
